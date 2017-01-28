ae7afdfc79d997b679d95f14765dd255eba42a14
ivrik <evylomova@student.unimelb.edu.au>
2017-01-28 04:03:42 +0300

    updated generate_NS.py

--- generate_NS.py
+++ generate_NS.py
@@ -1,37 +1,29 @@
 import codecs
 import random
+import sys
 def shuffle_relations(file_relations,oppos_out, shuff_out):
     lines=open(file_relations, 'r').readlines()
-    L=[]
-    Opp={}
-    for line in lines[1:]:
+    L, L2, Opp, L3 =[], {}, {}, {}
+    for line in lines:
 	## split pair of words
-        tokens=line.replace('\r\n','').replace('\n','').split(',')[0].split('+')[1].split('_')
-        L.append(tokens[0]+','+tokens[1])
-    for line in lines[1:]:
-        tokens=line.replace('\r\n','').replace('\n','').split(',')[0].split('+')
-	words=tokens[1].split('_')
-        if (words[1]+','+words[0]) not in L:
-            Opp[words[1]+','+words[0]]='oppos_'+tokens[0]
+        tokens=line.replace('\r\n','').replace('\n','').split(',')
+        L.append(tokens[1]+','+tokens[2])
+	if tokens[0] not in L2:
+		L2[tokens[0]]=[]
+	if tokens[1] not in L2[tokens[0]]:
+		L2[tokens[0]].append(tokens[1])
+	L3[tokens[1]+','+tokens[2]]=tokens[0]+','+tokens[1]+','+tokens[2]
+    for line in lines:
+        tokens=line.replace('\r\n','').replace('\n','').split(',')
+        if (tokens[2]+','+tokens[1]) not in L:
+            Opp[tokens[2]+','+tokens[1]]='oppos_'+tokens[0]
  
     writer=codecs.open(oppos_out,'w')
     for key,value in Opp.iteritems():
         writer.write(value+','+key+'\n')
     writer.close()
-
 	
-    lines=codecs.open(file_relations,'r').readlines()
-    L2={}
-    L3={}
     L4=[]
-    for line in lines[1:]:
-        tokens=line.replace('\n','').split(',')[0].split('+')
-	words=tokens[1].split('_')
-        if tokens[0] not in L2:
-            L2[tokens[0]]=[]
-        if tokens[1] not in L2[tokens[0]]:
-            L2[tokens[0]].append(words[0]) 
-        L3[words[0]+','+words[1]]=tokens[0]+','+words[0]+','+words[1]
     writer=codecs.open(shuff_out, 'w')
     print L2.keys()
     for key,value in L3.iteritems():
@@ -45,11 +37,9 @@ def shuffle_relations(file_relations,oppos_out, shuff_out):
                 	L4.append(result)
     writer.close()
 
-
+import getopt
 def main(argv):
-	rels=''
-	oppos=''
-	shuff=''
+	semantic=''
 	try:
 		opts, args = getopt.getopt(argv,"s:",["seman="])
 	except getopt.GetoptError:
@@ -57,9 +47,8 @@ def main(argv):
 		sys.exit(2)
 	for opt, arg in opts:
 		if opt in ("-s", "--seman"):
-			semantic=arg
-		
-	shuffle_relations(sematic,semantic+'.oppos.csv',semantic+'.shuff.csv')
+			semantic = arg
+	shuffle_relations(semantic,semantic+'.oppos.csv',semantic+'.shuff.csv')
 
 if __name__ == "__main__":
    main(sys.argv[1:])
